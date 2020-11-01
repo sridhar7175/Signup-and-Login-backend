308a666914ff071288d1cb258df122c5ca6fa37f
sridhar7175 <sridharreddy7175@gmail.com>
2020-11-01 10:28:41 +0530

    NewChanges

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,4 @@
 var productData = require("../models/product");
-// const formidable = require("formidable");
-// const _ = require("lodash");
-// const fs = require("fs");
 const multer = require("multer");
 const shortid = require("shortid");
 
@@ -14,51 +11,6 @@ exports.getproduct = (req, res) => {
       res.status(500).send(err);
     });
 };
-
-// exports.createproduct = (req, res) => {
-//   let form = new formidable.IncomingForm();
-//   form.keepExtensions = true;
-
-//   form.parse(req, (err, fields, file) => {
-//     if (err) {
-//       return res.status(400).json({
-//         error: "problem with image"
-//       });
-//     }
-//     //destructure the fields
-//     const { productName, productDescription, productPrice, productBrand} = fields;
-
-//     if (!productName || !productBrand || !productDescription || !productPrice) {
-//       return res.status(400).json({
-//         error: "Please include all fields"
-//       });
-//     }
-
-//     let product = new  productData.Product(fields);
-
-//     //handle file here
-//     if (file.photo) {
-//       if (file.photo.size > 4000000) {
-//         return res.status(400).json({
-//           error: "File size too big!"
-//         });
-//       }
-//       product.photo.data = fs.readFileSync(file.photo.path);
-//       product.photo.contentType = file.photo.type;
-//     }
-//     // console.log(product);
-
-//     //save to the DB
-//     product.save((err, product) => {
-//       if (err) {
-//         res.status(400).json({
-//           error: "Saving tshirt in DB failed"
-//         });
-//       }
-//       res.json(product);
-//     });
-//   });
-// };
 
 exports.createproduct = (req, res) => {
   // res.status(200).json({
@@ -113,40 +65,53 @@ exports.getoneproduct = (req, res) => {
 exports.updateproduct = (req, res) => {
   console.log(req.body);
   console.log(req.files);
+  const id = req.params.id;
 
-  // const {
-  //   productName,
-  //   productBrand,
-  //   productDescription,
-  //   productPrice,
-  // } = req.body;
+  const {
+    productName,
+    productBrand,
+    productDescription,
+    productPrice,
+  } = req.body;
 
-  // let productPicture = [];
+  let productPicture = [];
 
-  // if (req.files.length > 0) {
-  //   productPicture = req.files.map((file) => {
-  //     return { img: file.filename };
-  //   });
-  // }
+  if (req.files.length > 0) {
+    productPicture = req.files.map((file) => {
+      return { img: file.filename };
+    });
+  }
 
-  // const product = new productData.Product({
-  //   productName: productName,
-  //   productBrand,
-  //   productDescription,
-  //   productPrice,
-  //   productPicture,
-  // });
-
-  var id = req.params.id;
-  console.log(id);
-  //console.log(product);
-  productData.Product.findByIdAndUpdate(id, req.body, { new: true })
-    .then((products) => {
-      res.status(200).send(products);
+  productData.Product.findOneAndUpdate(
+    id,
+    {
+      $set: {
+        productName,
+        productBrand,
+        productDescription,
+        productPrice,
+        productPicture,
+      },
+    },
+    { new: true }
+  )
+    .then((post) => {
+      res.status(200).send(post);
     })
     .catch((err) => {
       res.status(500).send(err);
     });
+
+  // var id = req.params.id;
+  // console.log(id);
+  // //console.log(product);
+  // productData.Product.findByIdAndUpdate(id, req.body, { new: true })
+  //   .then((products) => {
+  //     res.status(200).send(products);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).send(err);
+  //   });
 };
 
 exports.deleteproduct = (req, res) => {
